2d991a243ee285d42495b276d6215d36a59b4b88
Nurul Fatihah <fatihahzahari248721@gmail.com>
2024-06-07 03:59:58 +0800

    first com

---
+++ update.php
@@ -0,0 +1,21 @@
+<?php
+include 'Database.php';
+include 'User.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $matric = $_POST['matric'];
+    $name = $_POST['name'];
+    $role = $_POST['role'];
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $user = new User($db);
+    $user->updateUser($matric, $name, $role);
+
+    $db->close();
+
+    // Redirect to read page after update
+    header("Location: read.php");
+}
+?>
